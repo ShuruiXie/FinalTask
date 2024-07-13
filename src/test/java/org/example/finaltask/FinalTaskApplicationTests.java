81affec1b4c1d4405d63c3e764e7860616b70d62
xsr <2829352230@qq.com>
2024-07-13 15:30:03 +0800

    原始项目

---
+++ src/test/java/org/example/finaltask/FinalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
